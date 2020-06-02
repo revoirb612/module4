9e40a1fbf1f1fdfe8b8cd5be1560367e33d45ec6
홍승일 <revoirb612@gmail.com>
2020-06-02 22:30:26 +0900

    Initial commit

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransaction;
+
+  Chart(this.recentTransaction);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      return {'day': 'T', 'amount': 9.99};
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
